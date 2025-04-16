a144abb64170e4ef6311d2444003b39dfaef6dbd
pinea <pinea@192.168.0.93>
2025-04-16 16:38:09 +0900

    hotelulu_1

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,11 +1,14 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.repository.HouseRepository;
@@ -13,7 +16,6 @@ import com.example.samuraitravel.repository.HouseRepository;
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
-
 	private final HouseRepository houseRepository;
 
 	public AdminHouseController(HouseRepository houseRepository) {
@@ -21,9 +23,20 @@ public class AdminHouseController {
 	}
 
 	@GetMapping
-	public String index(Model model) {
-		List<House> houses = houseRepository.findAll();
-		model.addAttribute("houses", houses);
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
+
+		Page<House> housePage;
+		if (keyword != null && !keyword.isEmpty()) {
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		} else {
+			housePage = houseRepository.findAll(pageable);
+		}
+
+		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword", keyword);
+
 		return "admin/houses/index";
 	}
 }
