d381d425989a8d1818e45ab4965dd7e18c87b769
pinea <pinea@192.168.0.93>
2025-04-21 17:56:37 +0900

    hotelulu_01_25_会員情報を閲覧・編集できるようにしよう

--- samuraitravel/src/main/java/com/example/samuraitravel/service/UserService.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.samuraitravel.SignupForm;
 import com.example.samuraitravel.entity.Role;
 import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.form.UserEditForm;
 import com.example.samuraitravel.repository.RoleRepository;
 import com.example.samuraitravel.repository.UserRepository;
 
@@ -35,10 +36,23 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		//user.setEnabled(true);
 		user.setEnabled(false);
 
 		return userRepository.save(user);
+	}
+
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+		User user = userRepository.getReferenceById(userEditForm.getId());
+
+		user.setName(userEditForm.getName());
+		user.setFurigana(userEditForm.getFurigana());
+		user.setPostalCode(userEditForm.getPostalCode());
+		user.setAddress(userEditForm.getAddress());
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+		user.setEmail(userEditForm.getEmail());
+
+		userRepository.save(user);
 	}
 
 	//メールアドレスが登録済みかどうかをチェックする
@@ -57,5 +71,12 @@ public class UserService {
 	public void enableUser(User user) {
 		user.setEnabled(true);
 		userRepository.save(user);
+	}
+
+	//メールアドレスが変更されたかどうかをチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
 	}
 }
