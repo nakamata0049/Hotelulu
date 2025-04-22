8deb61293915e115cfc1aa147284c636b3d316c1
pinea <pinea@192.168.0.93>
2025-04-22 16:30:52 +0900

    hotelulu_01_28_並べ替え機能を作成しよう

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -12,4 +12,12 @@ public interface HouseRepository extends JpaRepository<House, Integer> {
 	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
 	public Page<House> findByAddressLike(String area, Pageable pageable);
 	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+	public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);
+	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword, Pageable pageable);
+	public Page<House> findByAddressLikeOrderByCreatedAtDesc(String area, Pageable pageable);
+	public Page<House> findByAddressLikeOrderByPriceAsc(String area, Pageable pageable);
+	public Page<House> findByPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+	public Page<House> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable);
+	public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
+	public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
 }
